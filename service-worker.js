0d69f9546ef83fecd309e9a964cb43554d2a2d22
miguel19922025 <mfg_1992@hotmail.com>
2025-07-12 16:41:59 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,15 @@
 // service-worker.js
-const CACHE_NAME = 'fuel-converter-v1';
+const CACHE_NAME = 'gee-app-v1';
 const urlsToCache = [
-  '/combus/', // La raíz de tu aplicación ahora será /combus/
-  '/combus/index.html',
-  '/combus/manifest.json',
-  '/combus/service-worker.js',
+  // IMPORTANTE: Reemplaza YOUR_GITHUB_REPO_NAME con el nombre de tu repositorio de GitHub (ej. GEE-App)
+  '/YOUR_GITHUB_REPO_NAME/', // La raíz de tu aplicación ahora será /YOUR_GITHUB_REPO_NAME/
+  '/YOUR_GITHUB_REPO_NAME/index.html',
+  '/YOUR_GITHUB_REPO_NAME/manifest.json',
+  '/YOUR_GITHUB_REPO_NAME/service-worker.js',
   'https://cdn.tailwindcss.com', // Cacha también Tailwind CSS
   // Agrega aquí todas las rutas a tus iconos
-  '/combus/icons/icon-192x192.png',
-  '/combus/icons/icon-512x512.png'
+  '/YOUR_GITHUB_REPO_NAME/icons/gee-icon-192x192.png',
+  '/YOUR_GITHUB_REPO_NAME/icons/gee-icon-512x512.png'
 ];
 
 self.addEventListener('install', (event) => {
@@ -40,7 +41,6 @@ self.addEventListener('activate', (event) => {
       return Promise.all(
         cacheNames.map((cacheName) => {
           if (cacheWhitelist.indexOf(cacheName) === -1) {
-                // Elimina cachés antiguas que no están en la lista blanca
             return caches.delete(cacheName);
           }
         })
