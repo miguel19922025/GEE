9d0037bea2ed4b4c00093069aa69d96821281c82
miguel19922025 <mfg_1992@hotmail.com>
2025-07-12 13:24:19 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,30 @@
- service-worker.js
+// service-worker.js
 const CACHE_NAME = 'fuel-converter-v1';
 const urlsToCache = [
-  'combus',  La raíz de tu aplicación ahora será combus
-  'combusindex.html',
-  'combusmanifest.json',
-  'combusservice-worker.js',
-  'httpscdn.tailwindcss.com',  Cacha también Tailwind CSS
-   Agrega aquí todas las rutas a tus iconos
-  'combusiconsicon-192x192.png',
-  'combusiconsicon-512x512.png'
+  '/combus/', // La raíz de tu aplicación ahora será /combus/
+  '/combus/index.html',
+  '/combus/manifest.json',
+  '/combus/service-worker.js',
+  'https://cdn.tailwindcss.com', // Cacha también Tailwind CSS
+  // Agrega aquí todas las rutas a tus iconos
+  '/combus/icons/icon-192x192.png',
+  '/combus/icons/icon-512x512.png'
 ];
 
-self.addEventListener('install', (event) = {
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) = {
+      .then((cache) => {
         console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
 });
 
-self.addEventListener('fetch', (event) = {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then((response) = {
+      .then((response) => {
         if (response) {
           return response;
         }
@@ -33,14 +33,14 @@ self.addEventListener('fetch', (event) = {
   );
 });
 
-self.addEventListener('activate', (event) = {
+self.addEventListener('activate', (event) => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
-    caches.keys().then((cacheNames) = {
+    caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.map((cacheName) = {
+        cacheNames.map((cacheName) => {
           if (cacheWhitelist.indexOf(cacheName) === -1) {
-                 Elimina cachés antiguas que no están en la lista blanca
+                // Elimina cachés antiguas que no están en la lista blanca
             return caches.delete(cacheName);
           }
         })
